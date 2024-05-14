b449886db0d6a7f802560beb9d90d9429db77e1d
Francisco Procópio <francisco.procopio@enacom.com.br>
2024-05-14 10:51:58 -0300

    remove elements from tree

--- Trees/binaryTree.py
+++ Trees/binaryTree.py
@@ -74,6 +74,60 @@ class BinaryTree:
                 current = current.right_child
 
         return (parent, current)
+    
+    def remove(self, data):
+        parent, node = self.get_node_with_parent(data)
+
+        if parent is None and node is None:
+            return False
+        
+        # Get children count
+        children_count = 0
+        if node.left_child and node.right_child:
+            children_count = 2
+        elif (node.left_child is None) and (node.right_child is None):
+            children_count = 0
+        else:
+            children_count = 1
+
+        if children_count == 0:
+            if parent:
+                if parent.right_child is node:
+                    parent.right_child = None
+                else:
+                    parent.left_child = None
+            else:
+                self.root_node = None
+        elif children_count == 1:
+            next_node = None
+            if node.left_child:
+                next_node = node.left_child
+            else:
+                next_node = node.right_child
+
+            if parent:
+                if parent.left_child is node:
+                    parent.left_child = next_node
+                else:
+                    parent.right_child = next_node
+            else:
+                self.root_node = next_node
+        else:
+            parent_of_leftmost_node = node
+            leftmost_node = node.right_child
+            while leftmost_node.left_child:
+                parent_of_leftmost_node = leftmost_node
+                leftmost_node = leftmost_node.left_child
+
+            node.data = leftmost_node.data
+
+            if parent_of_leftmost_node.left_child == leftmost_node:
+                parent_of_leftmost_node.left_child = leftmost_node.right_child
+            else:
+                parent_of_leftmost_node.right_child = leftmost_node.right_child
+
+
+    
 
 if __name__ == '__main__':
     tree = BinaryTree()
@@ -86,5 +140,12 @@ if __name__ == '__main__':
 
     pos = tree.search(9)
     print(f'Path: {pos}')
+
+    tree.remove(9)
+
+    pos = tree.search(9)
+    print(f'Path: {pos}')
+
+
 
 
